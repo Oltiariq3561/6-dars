2392c31c84a2ee028a736639b6871a14e1227ca2
Oltiariq3561 <oltiariq356@gmail.com>
2024-05-29 00:24:39 -0700

    tugadi

---
+++ index.js
@@ -0,0 +1,107 @@
+// let n = + prompt("son");
+
+// let sum = 0;
+
+// let count = 0
+
+// for (let i = 1; i <= n; i++) {
+//     sum = sum + i ** 3;
+//     console.log(sum);
+// }
+
+
+// for (let i = 1; i <= n; i++) {
+//     if (n % i == 0) {
+//         count = count + i
+//         console.log(count);
+//     }
+
+
+// }
+
+
+// if (count % 2 == 1) {
+//     console.log("qiziqarli");
+// } else {
+//     console.log("qiziqarli emas");
+// }
+
+
+
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// let result = 0;
+
+// for (let i = a; i <= b; i++) {
+//     if (i % 3 == 0 && i % 7 != 0) {
+//         result = result + i;
+//     }
+// }
+// console.log(result);
+
+
+
+// 1 - masala
+
+// let n = 50;
+
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i * i);
+//     }
+// }
+
+// 2 - masala
+// let n = 10
+// for (let i = 1; i <= n; i++) {
+//     console.log(i * i * i);
+// }
+
+// 3 - masala
+// let n = 20
+// let faktor=0
+
+// for (let i = 1; i <= n; i++) {
+// faktor+=i;
+// }
+// console.log(faktor);
+
+
+// 4 - masala
+// let n =30
+// let result = 0;
+// for (let i = 1; i <= n; i++) {
+//     result += i * i;
+// }
+// console.log(result);
+
+// 6-masala
+// let n = 100
+// for (let i = 1; i <= n; i++){
+// if (i * 2 < n) {
+//     console.log(i);
+// }
+// }
+
+// 7-masala
+// let n = 100
+
+// for (let i = 1; i <= n; i++){
+//     if (i % 7 == 0 && i % 5 !== 0) {
+//         console.log(i);
+//     }
+//     }
+
+// 8-masala
+
+// let n = + prompt("son")
+// let result = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (n%i==0){
+//     result+=i;
+//     }
+// }
+// console.log(result);
